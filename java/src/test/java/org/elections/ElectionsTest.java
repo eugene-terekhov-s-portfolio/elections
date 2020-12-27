ef3129a2163d3563bc4b13a443ec2bf9580c11a2
nergal-perm <nergal@psu.ru>
2020-12-27 19:55:45 +0500

    Additional elector to kill 3 test mutants

--- java/src/test/java/org/elections/ElectionsTest.java
+++ java/src/test/java/org/elections/ElectionsTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +15,8 @@ class ElectionsTest {
         Map<String, List<String>> list = Map.of(
                 "District 1", Arrays.asList("Bob", "Anna", "Jess", "July"),
                 "District 2", Arrays.asList("Jerry", "Simon"),
-                "District 3", Arrays.asList("Johnny", "Matt", "Carole")
+                "District 3", Arrays.asList("Johnny", "Matt", "Carole"),
+                "District 4", Collections.singletonList("Eugene")
         );
         Elections elections = new Elections(list, false);
         elections.addCandidate("Michel");
@@ -29,6 +31,7 @@ class ElectionsTest {
         elections.voteFor("Jess", "Joe", "District 1");
         elections.voteFor("Simon", "", "District 2");
         elections.voteFor("Carole", "", "District 3");
+        elections.voteFor("Eugene", "", "District 4");
 
         Map<String, String> results = elections.results();
 
@@ -36,9 +39,9 @@ class ElectionsTest {
                 "Jerry", "50,00%",
                 "Johnny", "50,00%",
                 "Michel", "0,00%",
-                "Blank", "25,00%",
-                "Null", "25,00%",
-                "Abstention", "11,11%");
+                "Blank", "33,33%",
+                "Null", "22,22%",
+                "Abstention", "10,00%");
         Assertions.assertThat(results).isEqualTo(expectedResults);
     }
 
@@ -47,7 +50,8 @@ class ElectionsTest {
         Map<String, List<String>> list = Map.of(
                 "District 1", Arrays.asList("Bob", "Anna", "Jess", "July"),
                 "District 2", Arrays.asList("Jerry", "Simon"),
-                "District 3", Arrays.asList("Johnny", "Matt", "Carole")
+                "District 3", Arrays.asList("Johnny", "Matt", "Carole"),
+                "District 4", Collections.singletonList("Eugene")
         );
         Elections elections = new Elections(list, true);
         elections.addCandidate("Michel");
@@ -55,24 +59,25 @@ class ElectionsTest {
         elections.addCandidate("Johnny");
 
         elections.voteFor("Bob", "Jerry", "District 1");
-        elections.voteFor("Jerry", "Jerry", "District 2");
         elections.voteFor("Anna", "Johnny", "District 1");
-        elections.voteFor("Johnny", "Johnny", "District 3");
-        elections.voteFor("Matt", "Donald", "District 3");
         elections.voteFor("Jess", "Joe", "District 1");
         elections.voteFor("July", "Jerry", "District 1");
+        elections.voteFor("Jerry", "Jerry", "District 2");
         elections.voteFor("Simon", "", "District 2");
+        elections.voteFor("Johnny", "Johnny", "District 3");
+        elections.voteFor("Matt", "Michel", "District 3");
         elections.voteFor("Carole", "", "District 3");
+        elections.voteFor("Eugene", "Michel", "District 4");
 
         Map<String, String> results = elections.results();
 
         Map<String, String> expectedResults = Map.of(
                 "Jerry", "66,67%",
-                "Johnny", "33,33%",
-                "Michel", "0,00%",
+                "Johnny", "0,00%",
+                "Michel", "33,33%",
                 "Blank", "22,22%",
-                "Null", "22,22%",
-                "Abstention", "0,00%");
+                "Null", "11,11%",
+                "Abstention", "10,00%");
         Assertions.assertThat(results).isEqualTo(expectedResults);
     }
 }
